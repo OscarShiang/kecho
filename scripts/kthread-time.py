21aa0e1e9c5bada63124272f20739b8e18010f00
Oscar Shiang <oscar0225@livemail.tw>
2020-04-25 13:59:13 +0800

    Revise the probe script

--- scripts/kthread-time.py
+++ scripts/kthread-time.py
@@ -1,33 +1,31 @@
-#!/usr/bin/env python3
-
 from bcc import BPF
 code = """
 #include <uapi/linux/ptrace.h>
 
-BPF_HASH(start, u64, u64);
+BPF_HASH(start, u64);
 
 int probe_handler(struct pt_regs *ctx)
 {
 	u64 ts = bpf_ktime_get_ns();
-	u64 pid = bpf_get_currect_pid_tgid();
-	start.update(&pid, &ts);
+	bpf_trace_printk("in %llu\\n",ts);
 	return 0;
 }
 
-int ret_handler(struct pt_regs *ctx)
+int end_function(struct pt_regs *ctx)
 {
 	u64 ts = bpf_ktime_get_ns();
-	u64 pid = bpf_get_currect_pid_tgid();
-	u64 *tsp = (start.lookup(&pid));
-	if (tsp) {
-		bpf_trace_printk("%llu\\n", ts - *tsp);
-		start.delete(&pid);
-	}
+	bpf_trace_printk("out %llu\\n",ts);
 	return 0;
 }
 """
 
 b = BPF(text = code)
-b.attach_kprobe(event = "kthread_run", fn_name = "probe_handler")
-b.attach_kretprobe(event = "kthread_run", fn_name = "ret_handler")
-b.trace_print()
+b.attach_kprobe(event = 'my_thread_run', fn_name = 'probe_handler')
+b.attach_kretprobe(event = 'my_thread_run', fn_name = 'end_function')
+
+while True:
+	try:
+		res = b.trace_fields()
+	except ValueError:
+		continue
+	print(res[5].decode("UTF-8"))
